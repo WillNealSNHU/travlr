94a20ab5523256c2afd2b46ae85479a2830e6bb7
WillNealSNHU <william.neal@snhu.edu>
2024-04-07 16:18:57 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//defing routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+require('./app_api/models/db');
 
 var app = express();
 
@@ -27,9 +32,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,52 @@
+
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
+
+//var fs = require('fs');
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+// get the travel view
+
+const travel = async function(req, res, next) {
+    console.log('Travel controller starting here');
+
+    await fetch(tripsEndpoint, options)
+
+    .then(res => res.json())
+
+    .then(json => {
+        console.log(json)
+    res.render('travel', {title: 'Travlr Getaways', trips: json});
+    })
+    .catch(err => res.status(500).send(e.message));
+};
+    module.exports = { travel };
+    
+
+
+
+/*
+This is the last code I had in here, I tried commenting out 
+a couple lines and just slightly tweaking according to the
+guide pdf but honestly that code really outdated or straight 
+up doesn't work. Not sure who is the head of the department
+but they really should look at updating a lot of those documents.
+Like from what I read it doesn't state anywhere to install node fetch..
+I was able to figure it out, and thats part of being a developer but for someone
+who isnt already working in the field it might take them a long time and a lot of 
+frustration to figure out what's wrong with the code. 
+
+
 const path = require('path');
-const fs = require('fs');
+//const fs = require('fs');
 
 const tripsFilePath = path.join(__dirname, '../data/trips.json');
-const trips = JSON.parse(fs.readFileSync(tripsFilePath, 'utf8'));
+//const trips = JSON.parse(fs.readFileSync(tripsFilePath, 'utf8'));
 
 const travel = (req, res) => {
     res.render('travel', { title: 'Travlr Getaways', trips: trips});
@@ -11,7 +55,7 @@ const travel = (req, res) => {
 module.exports = {
     travel
 };
-
+*/ 
 
 
 /*
